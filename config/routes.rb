a612c31843c9f114bb6ff50e05d41c0a7f232ef1
Victor Korelsky <victor.korelsky@mail.mcgill.ca>
2017-11-19 17:47:01 -0500

    axed useless views, moving to a login with facebook only ting

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  root to: 'pages#home'
+  authenticated :user do
+    # for now similar but will change once the rest is added
+    root to: 'pages#landing'
+  end
+
+  root to: 'pages#landing'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
