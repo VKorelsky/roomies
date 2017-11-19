d0f33b359be1a04a5b4ef5a7004206200e54ca87
Victor Korelsky <victor.korelsky@mail.mcgill.ca>
2017-11-19 17:39:12 -0500

    removed useless (for now) sign up routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, omniauth_providers: ['facebook']
+  devise :timeoutable , :rememberable, :trackable, :omniauthable, omniauth_providers: ['facebook']
 
   def self.find_for_facebook_oauth(auth)
      user_params = auth.slice(:provider, :uid)
